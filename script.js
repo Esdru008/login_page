0e59bc8e92d7738eed57a32323c627ae51f027de
Esdru008 <148643139+Esdru008@users.noreply.github.com>
2023-10-25 16:59:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function validateForm() {
-            const username = document.getElementById("username").value;
-            const password = document.getElementById("password").value;
-            const loginMessage = document.getElementById("loginMessage");
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        const loginMessage = document.getElementById("loginMessage");
 
-            if (username === "Cliente" && password === "1234") {
-                // Login correto
-                return true;
-            } else {
-                // Exibir mensagem de erro
-                loginMessage.textContent = "Usuário ou senha incorreta!!!";
-                return false; // Impede o envio do formulário
-            }
+        if (username === "Cliente" && password === "1234") {
+            // Redirecionar para a página de vídeo
+            window.location.href = "./video_page/video_page.html";
+            return false; // Impede o envio do formulário
+        } else {
+            // Exibir mensagem de erro
+            loginMessage.textContent = "Usuário ou senha incorreta!!!";
+            loginMessage.classList.add("shake"); // Adicionar classe de animação
+
+            // Remover a classe "shake" após um tempo (por exemplo, 0.5 segundos)
+            setTimeout(function() {
+                loginMessage.classList.remove("shake");
+            }, 500);
+
+            return false; // Impede o envio do formulário
         }
+    }
